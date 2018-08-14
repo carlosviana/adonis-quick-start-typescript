2b8268516795c47a71930a46986ad3a3ec3f8018
tiansin <tiansin@tiansin.com>
2018-08-14 15:27:22 +0800

    adjust submodule

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="./adonis/index.d.ts" />
-/// <reference path="./adonis/module.d.ts" />
-/// <reference path="./adonis/cache.d.ts" />
+/// <reference path="./@adonisjs/index.d.ts" />
+/// <reference path="./@adonisjs/module.d.ts" />
+/// <reference path="./@adonisjs/cache.d.ts" />
 
 //Custom defined.
